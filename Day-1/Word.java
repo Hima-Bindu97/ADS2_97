64a5c5aadb0a8fafb32232a7c7f1f9c82884d230
Hima-Bindu97 <himabindureddygotike@msitprogram.net>
2020-01-07 13:42:59 +0530

    done

--- Day-1/Word.java
+++ Day-1/Word.java
@@ -1,20 +1,28 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
-public class Word{
-    public static void main(String[] args) throws IOException {
-        File f=new File("C:\\Users\\DELL\\Desktop\\ADS_97\\ADS2_97\\Day-1\\synsets.txt");
-        Scanner sc=new Scanner(f);
-        while(sc.hasNextLine()){
-            String[] a=sc.nextLine().split(",");
+public class Word {
+    static File f = new File("C:\\Users\\DELL\\Desktop\\ADS_97\\ADS2_97\\Day-1\\synsets.txt");
+    public void synsets(File f) throws FileNotFoundException {
+        Scanner sc = new Scanner(f);
+        while (sc.hasNextLine()) {
+            String[] a = sc.nextLine().split(",");
             System.out.println(a[0]);
             System.out.println(a[1]);
         }
-        File f1=new File("C:\\Users\\DELL\\Desktop\\ADS_97\\ADS2_97\\Day-1\\hypernyms.txt");
-        Scanner sc1=new Scanner(f1);
-        while(sc1.hasNextLine()){
-            String[] b=sc1.nextLine().split(",");
+    }
+    static File f1 = new File("C:\\Users\\DELL\\Desktop\\ADS_97\\ADS2_97\\Day-1\\hypernyms.txt");
+    public void hypernyms(File f1) throws FileNotFoundException {
+        Scanner sc1 = new Scanner(f1);
+        while (sc1.hasNextLine()) {
+            String[] b = sc1.nextLine().split(",");
             System.out.println(b[0]);
         }
+    }
+    public static void main(String[] args) throws IOException {
+        Word w = new Word();
+        w.synsets(f);
+        w.hypernyms(f1);
     }
 }
